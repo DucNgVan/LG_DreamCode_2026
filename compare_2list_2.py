f85618205f76c28ccd4bfff38b77b283cc2b5e54
DucNgVan <ducnv1792005@gmail.com>
2026-05-04 11:58:00 +0700

    way 2 to compare 2 list

---
+++ compare_2list_2.py
@@ -0,0 +1,24 @@
+a = ['a','b','c',5,7.6,True,'c']
+b = ['a','c',5,7.6,True,'b','b']
+
+if len(a) > len(b) or len(b) > len(a):
+    print('False')
+else:
+    ans_a={}
+    ans_b={}
+
+    for i in a:
+        if i not in ans_a:
+            ans_a[i] = 1
+        else:
+            ans_a[i] += 1
+    for j in b:
+        if j not in ans_b:
+            ans_b[j] = 1
+        else:
+            ans_b[j] += 1
+
+    if ans_a == ans_b:
+        print('True')
+    else: 
+        print('False')
