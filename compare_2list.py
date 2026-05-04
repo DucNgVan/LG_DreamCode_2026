2008a3393b40ace5ab2af41227d908a6c7edeb66
DucNgVan <ducnv1792005@gmail.com>
2026-05-04 10:18:51 +0700

    Update_compare_2list
    update to compare a str with a lower or upper

--- compare_2list.py
+++ compare_2list.py
@@ -1,16 +1,28 @@
-def count_2list(n,m):
-    count_1 = [0] * 26
-    count_2 = [0] * 26
-    for i in n: 
-        count_1[ord(i)-ord('a')] += 1
+def count_2list(n, m):
+
+    count_1 = [0] * 52
+    count_2 = [0] * 52
+    
+
+    for i in n:
+        if i.islower():
+            count_1[ord(i) - ord('a')] += 1
+        elif i.isupper():
+            count_1[ord(i) - ord('A') + 26] += 1
+
     for i in m: 
-        count_2[ord(i)-ord('a')] += 1
-    for i in range(26):
+        if i.islower():
+            count_2[ord(i) - ord('a')] += 1
+        elif i.isupper():
+            count_2[ord(i) - ord('A') + 26] += 1
+            
+
+    for i in range(52):
         if count_1[i] != count_2[i]:
             return False
     return True
 
 if __name__ == '__main__':
-    n = list(map(str,input().split(',')))
-    m = list(map(str,input().split(',')))
-    print(count_2list(n,m))
+    n = input().split(',')
+    m = input().split(',')
+    print(count_2list(n, m))
